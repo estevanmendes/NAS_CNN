2e62ebdcda9167ec77507c507cffde9f4d31a6b7
estevan <estevan.amazonas@gmail.com>
2023-11-28 00:33:19 -0300

    step argument added

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ for c in {0..31}
 do
 if [ $((c%2)) -eq 0 ];
 then
-    python3.8 NAS_GA.py -sg $c --gpu 1;
+    python3.8 NAS_GA.py -sg $c --gpu 1 --steps 1;
 else
-    python3.8 NAS_GA.py -sg $c --gpu 2;
+    python3.8 NAS_GA.py -sg $c --gpu 2 --steps 1;
 fi
     sleep 1m
 done
