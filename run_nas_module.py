8531e2bb86dd7915a8d1bb67f74056364077d475
estevan <estevan.amazonas@gmail.com>
2023-12-05 02:39:41 -0300

    experiment 007 settings

--- run_nas_module.py
+++ run_nas_module.py
@@ -195,13 +195,13 @@ if __name__=="__main__":
         
     saving_generation=30
     testing=False
-    id_user='debug'#'teste_007'
+    id_user='teste_007'
     global id
     id=id_user#+str(datetime.datetime.now())
     max_depth=25
     generations=end_gen
-    population_size=3
-    num_of_evaluations=2
+    population_size=50
+    num_of_evaluations=3
     max_epochs=1
 
     sys.stdout = open(default_filenames[-1], '+a')
