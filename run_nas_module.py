2f8a048ec68b1949179248b5500916097c6fbac1
estevan <estevan.amazonas@gmail.com>
2023-12-08 14:01:32 -0300

    removing tree graph

--- run_nas_module.py
+++ run_nas_module.py
@@ -148,12 +148,12 @@ def main(id,max_depth,generations,population_size,start_gen,saving_generation,nu
         with open(f'id_{id}_logbook.txt','+a') as f:
             json.dump(log,f)
 
-        if start_gen==0:
-            graph = networkx.DiGraph(history.genealogy_tree)
-            graph = graph.reverse()     # Make the graph top-down
-            colors = [toolbox.evaluate(history.genealogy_history[i])[0] for i in graph]
-            networkx.draw(graph, node_color=colors)
-            plt.savefig(f'id_{id}_genealogy_tree.png')
+        # if start_gen==0:
+        #     graph = networkx.DiGraph(history.genealogy_tree)
+        #     graph = graph.reverse()     # Make the graph top-down
+        #     colors = [toolbox.evaluate(history.genealogy_history[i])[0] for i in graph]
+        #     networkx.draw(graph, node_color=colors)
+        #     plt.savefig(f'id_{id}_genealogy_tree.png')
 
         # files=[f'id_{id}_individuals_generation.txt',f'arquiteturas_validas_max_depth_{max_depth}.json']#,f'id_{id}_genealogy_tree.png']
         files=[f'arquiteturas_validas_max_depth_{max_depth}_size_{pool_size}.json',f'id_{id}_individuals_generation.txt',f'id_{id}_logbook.txt']
